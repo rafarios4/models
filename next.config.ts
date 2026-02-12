7ffcaf3b15e5a10d88a473b95f24efb100ec2877
Rafa Rios <rafa.rios.valera@gmail.com>
2026-02-12 13:03:32 +0100

    other commit

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,10 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+const isProd = process.env.NODE_ENV === 'production';
+module.exports = {
+    basePath: isProd ? '/models' : '', 
+    assetPrefix: isProd ? '/models/' : '',
+    images: { unoptimized: true} // GitHub Pages does not support Next.js image optimization
+}; 
